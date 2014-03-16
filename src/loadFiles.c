5a8c020bd7ece2a82c885b36dc1c744650c31a78
Zach Howell <Zach@Aesop.(none)>
2014-03-15 23:05:22 -0400

    Added comments to loadFiles.c

--- src/loadFiles.c
+++ src/loadFiles.c
@@ -9,7 +9,11 @@
 
 extern char * path;
 
-
+/* 
+ * @param: numSongs -old reference to numSongs global
+ * @param: fName - the file  to write to
+ * purpose: store loaded information to use in future sesssions
+*/
 void writeBinarySongs(int numSongs, char fName[])
 {
 
@@ -45,6 +49,11 @@ void writeBinarySongs(int numSongs, char fName[])
 	
 }
 
+/* 
+ * @param: fName - the file  to read From
+ * purpose: read information stored in previous runs for this one
+ * returns: number of songs read in
+*/
 int readBinarySongs(char fName[])
 {
 
@@ -86,6 +95,11 @@ int readBinarySongs(char fName[])
 }
 
 
+/*
+ * @param: numSongs - old reference to number of Songs global 
+ * @param: songNames - a list of song names
+ * purpose: create the global songList struct from a list of songNames
+*/
 void initializeSongs(int numSongs, char songNames[][LENGTH])
 {
 
@@ -93,27 +107,35 @@ void initializeSongs(int numSongs, char songNames[][LENGTH])
 	songList = (Song *) malloc ( sizeof(Song) * numSongs);
 
 	for (i = 0; i < numSongs; i++) {
+        /* give each song the correct name */
 		songList[i].name =malloc(sizeof(char) * (strlen(songNames[i]) + 1) ); 
 		strcpy((songList[i].name), (songNames[i]));
+		songList[i].nameLength = strlen(songList[i].name) + 1;
+
+        /* initialize markov chain array, with all values = 1 */ 
 		songList[i].markov = (int *) calloc (numSongs, sizeof(int));
 		for (j = 0; j < numSongs; j++)
 			songList[i].markov[j] = 1;
 		songList[i].markovLength = numSongs;
-		songList[i].nameLength = strlen(songList[i].name) + 1;
 
 	}
 	
 }
 
-//TODO: fix this function
+/*
+ * @param: songs - a list of song names to return
+ * @param: songsAdded - the number of songs we've added
+ * purpose: read all the song names from a file
+*/
 int getDirectory(char songs[NUMBER_OF_SONGS][LENGTH], int *songsAdded)
 {
+    /* get the file path from user and load into global path */
 	printf("Please enter the directory path with .mp3 files\n");
 	path = (char *) malloc( sizeof(char) * 100);
 	fgets(path, 100, stdin);
-	//printf("size: %d", strlen(path));
-	//strcpy(path, "/home/jab489/Music");	
 	strtok(path, "\n");
+
+    /* open the directory */
 	DIR *dp; //pointer to directory
 	struct dirent *ep; // directory entry
 
@@ -123,16 +145,17 @@ int getDirectory(char songs[NUMBER_OF_SONGS][LENGTH], int *songsAdded)
 		printf("cant open %s\n", path);
 		return(0);
 	}
-	//type 8 is mp3
+	//actually read the file names into songs and increment songNumber
 	else {
 		while ((ep = readdir(dp)) != NULL) {
-			if (ep->d_type == 8) {
+			if (ep->d_type == 8) { //type 8 is mp3
 				strcpy(songs[songNumber], ep->d_name);
 				songNumber++;
 			}
 		}
 	}
 
+    /* assign songNumber to songsAdded and sort the final array */
 	*songsAdded = songNumber;
 	qsort(songs, songNumber, LENGTH,
 		(int (*)(const void *, const void*)) strcmp);
